6f1ca02ecd55216c4bebef3afab03cc8684a530c
pengzhao-intel <patric.zhao@intel.com>
2017-07-22 17:22:06 +0800

    keras example

---
+++ keras_cnn.py
@@ -0,0 +1,43 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+#from keras.utils import plot_model
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+img_rows, img_cols = 28, 28
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+if K.image_data_format() == 'channels_first':
+    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+    input_shape = (1, img_rows, img_cols)
+else:
+    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+    input_shape = (img_rows, img_cols, 1)
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),activation='relu',input_shape=input_shape, name='conv'))
+model.add(MaxPooling2D(pool_size=(2, 2), name='pool'))
+model.add(Flatten())
+model.add(Dense(num_classes, activation='softmax', name='fc'))
+print model.summary()
+#plot_model(model, to_file='keras_cnn.png', show_shapes=True)
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+	optimizer=keras.optimizers.Adadelta(),metrics=['accuracy'])
+
+model.fit(x_train, y_train,batch_size=batch_size,epochs=epochs,
+          verbose=1,validation_data=(x_test, y_test))
